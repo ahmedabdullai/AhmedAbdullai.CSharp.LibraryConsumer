78556ec576c3151a1ce8d38a4a0bb01195fad216
Ahmed Abdullai <ahmedabdullai@outlook.com>
2024-09-12 13:21:53 +0200

    Add project files.

---
+++ AhmedAbdullai.CSharp.LibraryConsumer/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculators;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(BasicCalculator.Add(5,3));
+Console.WriteLine(BasicCalculator.Subtract(5,3));
+Console.WriteLine(BasicCalculator.Multiply(5,3));
+Console.WriteLine(BasicCalculator.Divide(5,3));
